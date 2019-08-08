ffda3e9f1557faf14a30d2098d0fef56ce98feee
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2019-08-08 09:10:41 +0200

    Use absolute paths for statics as per Next.js doc

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,9 @@ import css from 'sass/components/Footer.scss';
 
 const Footer = props => (
   <footer className={`${props.className ? `${props.className} ` : ''}${css.Footer}`}>
-    <img src="static/images/footer/L.png" className={css.left} alt="" />
-    <img src="static/images/footer/Water.png" className={css.water} alt="" />
-    <img src="static/images/footer/R.png" className={css.right} alt="" />
+    <img src="/static/images/footer/L.png" className={css.left} alt="" />
+    <img src="/static/images/footer/Water.png" className={css.water} alt="" />
+    <img src="/static/images/footer/R.png" className={css.right} alt="" />
   </footer>
 );
 
