f54b982d21b58b8b547582c4b19b99371bbe0f5f
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2019-08-11 14:07:42 +0200

    Optimise font size and loading order

--- components/PortfolioHead.jsx
+++ components/PortfolioHead.jsx
@@ -4,9 +4,6 @@ import PropTypes from 'prop-types';
 
 const PortfolioHead = props => (
   <Head>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-    <meta name=" theme-color" content="#1185c2" />
     <link rel="icon" sizes="192x192" href="/static/images/AppIcon.png" />
     <meta
       name="description"
---
+++ pages/_document.jsx
@@ -0,0 +1,24 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+          <meta name=" theme-color" content="#1185c2" />
+          <link rel="preload" href="/static/fonts/subset-Baloo.woff2" crossOrigin="true" as="font" />
+          <link rel="preload" href="/static/fonts/subset-MuseoSans-300.woff2" crossOrigin="true" as="font" />
+          <link rel="preload" href="/static/fonts/subset-Quicksand-Regular.woff2" crossOrigin="true" as="font" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
