5d8d4cc965e762fd60feb52b386abcbb2fe53ac3
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2019-08-14 16:32:33 +0200

    Integrate MDC text field for contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,9 @@
 import withLayout from 'components/_layout';
+import Button from 'components/Button';
+import Icon from 'components/Icon';
 import PortfolioHead from 'components/PortfolioHead';
+import TextField, { HelperText, Input } from '@material/react-text-field';
+import css from 'sass/components/react-text-field.scss';
 
 const Contact = () => (
   <>
@@ -12,8 +16,72 @@ const Contact = () => (
       {' or '}
       <a href="tel:+31123456789">by phone at +33 6 24 19 63</a>
       {'.'}
-
     </p>
+
+
+    <div className={css.TextField}>
+      <TextField
+        label="Name"
+        outlined
+      >
+        <Input
+          required
+          onChange={e => console.log({ value: e.currentTarget.value })}
+        />
+      </TextField>
+    </div>
+
+    <div className={css.TextField}>
+      <TextField
+        label="Company name"
+        outlined
+        helperText={<HelperText>Optional</HelperText>}
+      >
+        <Input
+          onChange={e => console.log({ value: e.currentTarget.value })}
+        />
+      </TextField>
+    </div>
+
+
+    <div className={css.TextField}>
+      <TextField
+        label="Email address"
+        outlined
+      >
+        <Input
+          type="email"
+          required
+          onChange={e => console.log({ value: e.currentTarget.value })}
+        />
+      </TextField>
+    </div>
+
+
+    <div className={css.TextField}>
+      <TextField
+        label="Message"
+        outlined
+        textarea
+      >
+        <Input
+          required
+          onChange={e => console.log({ value: e.currentTarget.value })}
+        />
+      </TextField>
+    </div>
+
+
+    <Button
+      icon="send"
+      type="submit"
+      disabled="false"
+      onClick={() => console.log('clicked!')}
+    >
+      Send Message
+    </Button>
+
+
   </>
 );
 
