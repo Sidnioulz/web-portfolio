7f10d24df1fa38b5ecc39797e805bd884e7911a8
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2019-08-15 18:43:52 +0200

    Add missing Portfolio Head to pub pages

--- pages/pub/[id].jsx
+++ pages/pub/[id].jsx
@@ -1,8 +1,10 @@
 import PortfolioPropTypes from 'data/portfolio-prop-types';
+import PortfolioHead from 'components/PortfolioHead';
 import json from 'data/publications.json';
 
 const Page = props => (
   <>
+    <PortfolioHead title={props.publication.title} />
     <h2>{props.publication.title}</h2>
     <blockquote>{props.publication.authors.join(' · ')}</blockquote>
     {props.publication.description.split('\n').map(p => (<p>{p}</p>))}
